75c81626bd0ce05d710bf0fc9fb6e3d29f6975f2
nkvuong <nkvuong>
2021-09-06 15:23:36 +0000

    added screenshots

--- setup_notebooks/1. Unity Catalog - AWS setup.scala
+++ setup_notebooks/1. Unity Catalog - AWS setup.scala
@@ -16,7 +16,12 @@
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC ## Create file uc-metastore-role.json
+// MAGIC ### Step 0: prepare json files for aws-cli (not required for console UI)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC #### uc-metastore-role.json
 // MAGIC 
 // MAGIC - replace $BUCKET with your S3 bucket name
 
@@ -41,6 +46,7 @@
             ],
             "Effect": "Allow"
         },
+      // add the following to access Databricks sample data
         {
             "Action": [
                 "s3:GetObject",
@@ -60,9 +66,9 @@
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC ## Create file uc-metastore-role-trust-rel.json.json
+// MAGIC #### uc-metastore-role-trust-rel.json
 // MAGIC 
-// MAGIC - use content below as is
+// MAGIC - Replace $DATABRICKS_ACCOUNT_ID with your Databricks account ID (not AWS)
 
 // COMMAND ----------
 
@@ -77,7 +83,7 @@
             "Action": "sts:AssumeRole",
             "Condition": {
                 "StringEquals": {
-                    "sts:ExternalId": "XXXXXXXXXX" // your databricks account ID, notice that it’s not aws account id
+                    "sts:ExternalId": "$DATABRICKS_ACCOUNT_ID" // your databricks account ID, notice that it’s not aws account id
                 }
             }
         }
@@ -87,29 +93,64 @@
 // COMMAND ----------
 
 // MAGIC %md
+// MAGIC ### Step 1: Create a new S3 bucket
 // MAGIC 
+// MAGIC Create a S3 storage bucket (`$BUCKET`). This S3 bucket is the default storage location for managed tables in Unity Catalog. Use a dedicated bucket for each metastore.
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### Step 2: Set up the IAM roles and policies
 // MAGIC 
-// MAGIC # Step 1 Create a new S3 bucket
+// MAGIC Create an IAM Role in the same AWS account as the S3 bucket and grant it access to the storage bucket. 
+
+// COMMAND ----------
+
+// MAGIC %md 
+// MAGIC #### Step 2.1: Create AWS IAM Role with the trust relationship
 // MAGIC 
-// MAGIC Create a S3 storage bucket ($BUCKET). This S3 bucket is the default storage location for managed tables in Unity Catalog. Use a dedicated bucket for each metastore.
-// MAGIC 
-// MAGIC # Setup 2 the IAM roles and policies
-// MAGIC 
-// MAGIC Create an IAM Role in the same AWS account as the S3 bucket and grant it access to the storage bucket.  
-// MAGIC 
-// MAGIC ### Step 2.1 Create AWS iam Role
+// MAGIC Either via aws-cli:
 // MAGIC 
 // MAGIC `aws iam create-role --role-name [your_prefix]-uc-metastore-role  --assume-role-policy-document file://uc-metastore-role-trust-rel.json`
 // MAGIC 
-// MAGIC ### Step 2.2 Create AWS iam policy
+// MAGIC or via the UI
+// MAGIC 
+// MAGIC ![](screenshots/create_role.png)
+// MAGIC 
+// MAGIC ![](screenshots/set_role.png)
+// MAGIC 
+// MAGIC ![](screenshots/set_trust.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC #### Step 2.2: Create AWS IAM Policy
+// MAGIC 
+// MAGIC Via aws-cli
 // MAGIC 
 // MAGIC `aws iam create-policy --policy-name [your_prefix]-uc-metastore-policy --policy-document file://uc-metastore-role.json `
 // MAGIC 
-// MAGIC ### Step 2.3 Setup trust relationship
-// MAGIC Get the policy arn from above step and then use it in the below command
+// MAGIC or via the UI
+// MAGIC 
+// MAGIC ![](screenshots/create_policy.png)
+// MAGIC 
+// MAGIC ![](screenshots/set_policy.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC #### Step 2.3: Attach IAM policy to the role
+// MAGIC Get the policy arn from above step and then use it in the below aws-cli command
 // MAGIC 
 // MAGIC `aws iam attach-role-policy --role-name [your_prefix]-uc-metastore-role --policy-arn "arn:aws:iam::[.....]:policy/[your_prefix]-uc-metastore-policy"`
 // MAGIC 
-// MAGIC ### Step 2.4 Verify that the policies are attached to the role
+// MAGIC or via the UI
+// MAGIC 
+// MAGIC ![](screenshots/attach_policy.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC #### Step 2.4: Verify that the policies are attached to the role
 // MAGIC 
 // MAGIC `aws iam list-attached-role-policies --role-name [your_prefix]-uc-metastore-role`
