51c87d41866aa2864336f9ff185307e4abdc75c7
Vuong Nguyen <44292934+nkvuong@users.noreply.github.com>
2022-01-22 11:05:26 +0000

    force user creations on error

--- terraform/aws/users.tf
+++ terraform/aws/users.tf
@@ -5,6 +5,7 @@ resource "databricks_user" "unity_users" {
   provider  = databricks.mws
   for_each  = toset(concat(var.databricks_users, var.databricks_unity_admins))
   user_name = each.key
+  force     = true
 }
 
 resource "databricks_group" "admin_group" {
@@ -20,8 +21,8 @@ resource "databricks_group_member" "admin_group_member" {
 }
 
 resource "databricks_user_role" "my_user_account_admin" {
-  provider  = databricks.mws
-  for_each  = toset(var.databricks_unity_admins)
-  user_id = databricks_user.unity_users[each.value].id
-  role    = "account_admin"
+  provider = databricks.mws
+  for_each = toset(var.databricks_unity_admins)
+  user_id  = databricks_user.unity_users[each.value].id
+  role     = "account_admin"
 }
