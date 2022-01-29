d91c2f62d800d03fa6df7415c602bb72fb5476c1
Vuong Nguyen <44292934+nkvuong@users.noreply.github.com>
2022-01-29 14:02:09 +0000

    remove table creation code

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -46,28 +46,3 @@ resource "databricks_schema" "quickstart_schema" {
   catalog_name = databricks_catalog.quickstart_catalog.name
   comment      = "A new Unity Catalog schema called quickstart_database"
 }
-
-resource "databricks_table" "quickstart_table" {
-  provider           = databricks.workspace
-  name               = "quickstart_table"
-  catalog_name       = databricks_catalog.quickstart_catalog.name
-  schema_name        = databricks_schema.quickstart_schema.name
-  table_type         = "MANAGED"
-  data_source_format = "DELTA"
-  storage_location   = ""
-  column {
-    name      = "id"
-    position  = 0
-    type_name = "INT"
-    type_text = "int"
-    type_json = "{\"name\":\"id\",\"type\":\"integer\",\"nullable\":true,\"metadata\":{}}"
-  }
-  column {
-    name      = "name"
-    position  = 1
-    type_name = "STRING"
-    type_text = "varchar(64)"
-    type_json = "{\"name\":\"name\",\"type\":\"varchar(64)\",\"nullable\":true,\"metadata\":{}}"
-  }
-  comment = "A new Unity Catalog table called quickstart_table"
-}
